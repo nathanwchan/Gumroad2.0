aa1d3d8081ded2a3f3ae361d4a6a9f5d019e72b9
Nate Chan <nchan87@gmail.com>
2020-09-08 16:40:32 -0700

    reorganize mock data load

--- Gumroad 2.0/Gumroad 2.0/Helpers/NetworkManager.swift
+++ Gumroad 2.0/Gumroad 2.0/Helpers/NetworkManager.swift
@@ -19,28 +19,29 @@ class NetworkManager {
         allProducts = []
         allContent = []
 
-        let id = UUID().uuidString
-        allProducts.append(
-            Product(id: id,
-                name: "POST — A Short Story of No Consequence at All (PDF + AUDIOBOOK)",
-                creatorName: "Shea Serrano",
-                image: URL(string: "https://static-2.gumroad.com/res/gumroad/3015050709675/asset_previews/fbf4925553912ef3aba708c2a380056e/retina/POST_20_E2_80_94_20Cover.jpg"),
-                archived: false)
-        )
-        allContent.append(
-            Content(id: UUID().uuidString,
-                    productId: id,
-                    name: "POST — A Short Story of No Consequence at All (PDF)",
-                    sizeKb: 132,
-                    url: URL(string: ""))
-        )
-        allContent.append(
-            Content(id: UUID().uuidString,
-                    productId: id,
-                    name: "POST — A Short Story of No Consequence at All (AUDIOBOOK)",
-                    sizeKb: 59832,
-                    url: URL(string: ""))
-        )
+        for product in MockData.products {
+            let id = UUID().uuidString
+            allProducts.append(
+                Product(
+                    id: id,
+                    name: product.name,
+                    creatorName: product.creatorName,
+                    image: URL(string: product.imageUrlString),
+                    archived: false
+                )
+            )
+            for content in product.content {
+                allContent.append(
+                    Content(
+                        id: UUID().uuidString,
+                        productId: id,
+                        name: content.name,
+                        sizeKb: content.sizeKb,
+                        url: URL(string: content.urlString)
+                    )
+                )
+            }
+        }
     }
 
     /* END - FOR MOCK PURPOSES ONLY */
--- Gumroad 2.0/Gumroad 2.0/Helpers/Utilities.swift
+++ Gumroad 2.0/Gumroad 2.0/Helpers/Utilities.swift
@@ -10,3 +10,50 @@ import Foundation
 enum Globals {
     static let AuthTokenUserDefaultsKey: String = "authToken"
 }
+
+enum MockData {
+    static let products: [ProductMockData] = [
+        ProductMockData(
+            name: "POST — A Short Story of No Consequence at All (PDF + AUDIOBOOK)",
+            creatorName: "Shea Serrano",
+            imageUrlString: "https://static-2.gumroad.com/res/gumroad/3015050709675/asset_previews/fbf4925553912ef3aba708c2a380056e/retina/POST_20_E2_80_94_20Cover.jpg",
+            content: [
+                ContentMockData(
+                    name: "POST — A Short Story of No Consequence at All (PDF)",
+                    sizeKb: 132,
+                    urlString: ""
+                ),
+                ContentMockData(
+                    name: "POST — A Short Story of No Consequence at All (AUDIOBOOK)",
+                    sizeKb: 59832,
+                    urlString: ""
+                )
+            ]
+        ),
+        ProductMockData(
+            name: "CONFERENCE ROOM, FIVE MINUTES — Ten Illustrated Essays About The Office",
+            creatorName: "Shea Serrano",
+            imageUrlString: "https://static-2.gumroad.com/res/gumroad/3015050709675/asset_previews/dd472bffe57a0a49860bc926f01dd76f/retina/Screen_20Shot_202020-07-18_20at_2011.52.37_20AM.png",
+            content: [
+                ContentMockData(
+                    name: "CONFERENCE ROOM, FIVE MINUTES — Ten Illustrated Essays About The Office (PDF)",
+                    sizeKb: 245,
+                    urlString: ""
+                )
+            ]
+        ),
+    ]
+}
+
+struct ProductMockData {
+    var name: String
+    var creatorName: String
+    var imageUrlString: String
+    var content: [ContentMockData]
+}
+
+struct ContentMockData {
+    var name: String
+    var sizeKb: Int
+    var urlString: String
+}
